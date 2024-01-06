522a53db8f90efe8d4a7619115fc0f75aac70dec
Vesteban <87298866+UVEsteban@users.noreply.github.com>
2024-01-06 19:06:43 +0000

    Changes to be committed: 	new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/cs50.c 	new file:   .vscode/cs50.h 	renamed:    Week1/helloworld -> Week0/helloworld 	renamed:    Week1/helloworld.c -> Week0/helloworld.c 	renamed:    Week1/helloworld.py -> Week0/helloworld.py 	new file:   Week1/cs50.c 	new file:   Week1/cs50.h 	new file:   Week1/phonebook.c

---
+++ Week1/phonebook.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+
+{
+    string name = get_string("What's your name? ");
+    int age= get_int("What's your age?" );
+    string phone = get_string ("What's your phonenumber");
+
+    printf("Age is %i. Name is %s. Phone Number is %s.", age, name, phone);
+}
